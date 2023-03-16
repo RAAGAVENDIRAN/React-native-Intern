1df444cc533016e0cf8823da6260d169e4d9e0d2
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-16 15:49:27 +0530

    Reanimated withtiming, withSpring, withRepeat

--- practice_animations/practice/App.js
+++ practice_animations/practice/App.js
@@ -1,26 +1,39 @@
 import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Animated,{
+import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
+  withSpring,
+  withRepeat,
 } from "react-native-reanimated";
 import { StatusBar } from "expo-status-bar";
 
 const SIZE = 100.0;
 
+// const handleRotation = (progress: Animated.SharedValue<number>) =>{
+//   'worklet';    //as it is javascript function specify it as worklet, so that it will xord
+//   return `${progress.value * 2 * Math.PI}rad`;
+// }
+
 function App() {
   const progress = useSharedValue(1); //useSharedValue allows us to create a value that could be handled from the worklets so that it could be handled from UI thread.
+  const scale = useSharedValue(2);
 
   const reanimatedStyle = useAnimatedStyle(() => {
     return {
       opacity: progress.value,
+      borderRadius: (progress.value * SIZE) / 2,
+      transform: [
+        { scale: scale.value },
+        { rotate: `${progress.value * 2 * Math.PI}rad` },
+      ],
     };
   }, []);
 
   useEffect(() => {
-    progress.value = withTiming(0, { duration: 5000 });
-    console.log("Raaga");
+    progress.value = withRepeat(withSpring(0.5), -1, true); // -1 indicates repeat unlimimted times.
+    scale.value = withRepeat(withSpring(1), -1, true); //true indiccates that the animation is reversed
   }, []);
 
   return (
