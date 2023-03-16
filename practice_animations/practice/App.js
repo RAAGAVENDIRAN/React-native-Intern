c31534c391164c12a133a316cc7b402987e63928
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-16 17:02:29 +0530

    Understanding basics of pangesture in Reanimated

--- practice_animations/practice/App.js
+++ practice_animations/practice/App.js
@@ -1,50 +1,69 @@
 import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+  panGestureEvent,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
   withSpring,
   withRepeat,
+  useAnimatedGestureHandler,
 } from "react-native-reanimated";
-import { StatusBar } from "expo-status-bar";
 
-const SIZE = 100.0;
-
-// const handleRotation = (progress: Animated.SharedValue<number>) =>{
-//   'worklet';    //as it is javascript function specify it as worklet, so that it will xord
-//   return `${progress.value * 2 * Math.PI}rad`;
-// }
+const SIZE = 100;
+const CIRCLE_RADIUS = SIZE * 2;
 
 function App() {
-  const progress = useSharedValue(1); //useSharedValue allows us to create a value that could be handled from the worklets so that it could be handled from UI thread.
-  const scale = useSharedValue(2);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
 
-  const reanimatedStyle = useAnimatedStyle(() => {
+  const pan = useAnimatedGestureHandler({
+    onStart: (event, context) => {
+      context.translateX = translateX.value; //context is used to save the starting value here.
+      context.translateY = translateY.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = event.translationX + context.translateX;
+
+      translateY.value = event.translationY + context.translateY;
+    },
+    onEnd: () => {
+      const distance = Math.sqrt(translateX.value ** 2 + translateY.value ** 2);
+      if (distance < CIRCLE_RADIUS + SIZE/2) {
+        translateX.value = withSpring(0);
+        translateY.value = withSpring(0);
+      }
+    },
+  });
+
+  const rStyle = useAnimatedStyle(() => {
     return {
-      opacity: progress.value,
-      borderRadius: (progress.value * SIZE) / 2,
       transform: [
-        { scale: scale.value },
-        { rotate: `${progress.value * 2 * Math.PI}rad` },
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
       ],
     };
-  }, []);
-
-  useEffect(() => {
-    progress.value = withRepeat(withSpring(0.5), -1, true); // -1 indicates repeat unlimimted times.
-    scale.value = withRepeat(withSpring(1), -1, true); //true indiccates that the animation is reversed
-  }, []);
+  });
 
   return (
-    <View style={styles.container}>
-      <Animated.View
-        style={[
-          { height: SIZE, width: SIZE, backgroundColor: "blue" },
-          reanimatedStyle,
-        ]}
-      ></Animated.View>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.circle}>
+          <PanGestureHandler onGestureEvent={pan}>
+            <Animated.View style={[styles.square, rStyle]}></Animated.View>
+          </PanGestureHandler>
+        </View>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
@@ -54,6 +73,22 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
+  },
+  square: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: "rgba(0,0,256,0.5)",
+    borderRadius: 20,
+  },
+  circle: {
+    width: CIRCLE_RADIUS * 2,
+    height: CIRCLE_RADIUS * 2,
+
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: CIRCLE_RADIUS,
+    borderWidth: 5,
+    borderColor: "rgba(0,0,256,0.5)",
   },
 });
 
