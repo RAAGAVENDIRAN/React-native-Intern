a15ee99df8ff38cf8aa70ddd71f4a3b6166f637d
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-16 15:25:58 +0530

    first program in reanimated

--- practice_animations/practice/App.js
+++ practice_animations/practice/App.js
@@ -1,45 +1,47 @@
-import React, { Component } from "react";
-import { StyleSheet, View, Animated, ScrollView } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Animated,{
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+import { StatusBar } from "expo-status-bar";
 
-export default class animations extends Component {
-  state = {
-    animation: new Animated.Value(0),
-  };
-  render() {
-    const backgroundInterpolate = this.state.animation.interpolate({
-      inputRange: [0, 3000],
-      outputRange: ["rgb(255,99,71)", "rgb(99,71,255)"],
-    });
+const SIZE = 100.0;
 
-    const backgroundStyle = {
-      backgroundColor: backgroundInterpolate,
+function App() {
+  const progress = useSharedValue(1); //useSharedValue allows us to create a value that could be handled from the worklets so that it could be handled from UI thread.
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
     };
-    return (
-      <View style={styles.container}>
-        <ScrollView
-          scrollEventThrottle={16}
-          onScroll={Animated.event([
-            {
-              nativeEvent: {
-                contentOffset: {
-                  y: this.state.animation,
-                },
-              },
-            },
-          ])}
-        >
-          <Animated.View style={[styles.content, backgroundStyle]} />
-        </ScrollView>
-      </View>
-    );
-  }
+  }, []);
+
+  useEffect(() => {
+    progress.value = withTiming(0, { duration: 5000 });
+    console.log("Raaga");
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          { height: SIZE, width: SIZE, backgroundColor: "blue" },
+          reanimatedStyle,
+        ]}
+      ></Animated.View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  content: {
-    height: 3000,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
