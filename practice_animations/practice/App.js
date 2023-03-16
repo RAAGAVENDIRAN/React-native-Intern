d430db4525fc3bfaa2f9df4c94f11a488bb408bc
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-16 11:59:51 +0530

    interpolate - practice

--- practice_animations/practice/App.js
+++ practice_animations/practice/App.js
@@ -1,99 +1,51 @@
-import React, { Component, useRef, useState } from "react";
-import { Button, Easing, TouchableOpacity, Dimensions } from "react-native";
-import { Animated, View, StyleSheet, Text, PanResponder } from "react-native";
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Button,
+  TouchableWithoutFeedback,
+} from "react-native";
 
-var { width, height } = Dimensions.get("window");
-export default class App extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      fadeValue: new Animated.Value(0),
-      xValue: new Animated.Value(0),
-    };
-  }
-
-  // _fadeAnimation = () => {
-  //   Animated.timing(this.state.fadeValue, {
-  //     toValue: 1,
-  //     duration: 2000,
-  //   }).start();
-  // };
-
-  // _fadeAnimationReverse = () => {
-  //   Animated.timing(this.state.fadeValue, {
-  //     toValue: 0,
-  //     duration: 2000,
-  //   }).start();
-  // };
-
-  _MoveRight = () => {
-    Animated.timing(this.state.xValue, {
-      toValue: width - 100,
-      duration: 2000,
-
-      asing: Easing.cubic,
-    }).start(() => {
-      Animated.timing(this.state.xValue, {
-        toValue: 0,
-        duration: 1000,
-
-        asing: Easing.linear,
-      }).start();
-    });
+export default class animations extends Component {
+  state = {
+    animation: new Animated.Value(0),
   };
 
+  componentDidMount() {
+    Animated.timing(this.state.animation, {
+      toValue: 1,
+      duration: 4000,
+    }).start();
+  }
   render() {
+    const boxStyle = {
+      backgroundColor: this.state.animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: ["rgba(255,99,71, 1)", "blue"],
+      }),
+    };
+
     return (
       <View style={styles.container}>
-        {/* <Animated.View
-          style={[styles.animationView, { opacity: this.state.fadeValue }]}
-        ></Animated.View> */}
-        <Animated.View
-          style={[
-            styles.animationView,
-            { left: this.state.xValue },
-            // { opacity: this.state.fadeValue },
-          ]}
-        ></Animated.View>
-
-        {/* <TouchableOpacity style={styles.button} onPress={this._fadeAnimation}>
-          <Text style={styles.buttonText}>Animate</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={this._fadeAnimationReverse}
-        >
-          <Text style={styles.buttonText}>Animate Back</Text>
-        </TouchableOpacity> */}
-        <TouchableOpacity style={styles.button} onPress={this._MoveRight}>
-          <Text style={styles.buttonText}>Move in X Axis Right</Text>
-        </TouchableOpacity>
+        <Animated.View style={[styles.box, boxStyle]}></Animated.View>
+        <Button title="click color change" onPress={this.StartAni}></Button>
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: "center",
+    alignItems: "center",
     justifyContent: "center",
   },
-  animationView: {
-    height: 140,
-    width: 140,
+  box: {
+    height: 100,
+    width: 100,
     backgroundColor: "red",
-  },
-  buttonText: {
-    fontSize: 18,
-
-    fontWeight: "bold",
-    color: "white",
-    padding: 12,
-  },
-  button: {
-    backgroundColor: "steelblue",
-    height: 45,
-    marginTop: 20,
-    alignSelf: "center",
   },
 });
