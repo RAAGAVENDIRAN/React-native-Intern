9b677eb8cc2d1c88cd52b5f4dc63f0c0c756c229
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-16 11:24:42 +0530

    Moving through x-axis using class component

--- practice_animations/practice/App.js
+++ practice_animations/practice/App.js
@@ -1,65 +1,99 @@
-import React, { useRef, useState } from "react";
-import { Button } from "react-native";
+import React, { Component, useRef, useState } from "react";
+import { Button, Easing, TouchableOpacity, Dimensions } from "react-native";
 import { Animated, View, StyleSheet, Text, PanResponder } from "react-native";
 
-const App = () => {
-  const opacity = useRef(new Animated.Value(1)).current;
+var { width, height } = Dimensions.get("window");
+export default class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      fadeValue: new Animated.Value(0),
+      xValue: new Animated.Value(0),
+    };
+  }
 
-  return (
-    <View style={styles.container}>
-      <Animated.View
-        style={[
-          { width: 100, height: 100, backgroundColor: "red" },
-          { opacity: opacity },
-        ]}
-      />
-      <Button
-        title="Press Me"
-        onPress={() => {
-          Animated.timing(opacity, {
-            toValue: 0,
-            duration: 2000,
-            useNativeDriver: false,
-          }).start();
-        }}
-      />
-      <Button
-        title="Press Me"
-        onPress={() => {
-          Animated.timing(opacity, {
-            toValue: 1,
-            duration: 1000,
-            useNativeDriver: true,
-          }).start();
-        }}
-      />
-      {/* <Text style={styles.titleText}>Drag & Release this box!</Text>
-      <Animated.View
-        style={{ transform: [{ translateX: pan.x }, { translateY: pan.y }] }}
-        {...panResponder.panHandlers}
-      >
-        <View style={styles.box} />
-      </Animated.View> */}
-    </View>
-  );
-};
+  // _fadeAnimation = () => {
+  //   Animated.timing(this.state.fadeValue, {
+  //     toValue: 1,
+  //     duration: 2000,
+  //   }).start();
+  // };
 
+  // _fadeAnimationReverse = () => {
+  //   Animated.timing(this.state.fadeValue, {
+  //     toValue: 0,
+  //     duration: 2000,
+  //   }).start();
+  // };
+
+  _MoveRight = () => {
+    Animated.timing(this.state.xValue, {
+      toValue: width - 100,
+      duration: 2000,
+
+      asing: Easing.cubic,
+    }).start(() => {
+      Animated.timing(this.state.xValue, {
+        toValue: 0,
+        duration: 1000,
+
+        asing: Easing.linear,
+      }).start();
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <Animated.View
+          style={[styles.animationView, { opacity: this.state.fadeValue }]}
+        ></Animated.View> */}
+        <Animated.View
+          style={[
+            styles.animationView,
+            { left: this.state.xValue },
+            // { opacity: this.state.fadeValue },
+          ]}
+        ></Animated.View>
+
+        {/* <TouchableOpacity style={styles.button} onPress={this._fadeAnimation}>
+          <Text style={styles.buttonText}>Animate</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this._fadeAnimationReverse}
+        >
+          <Text style={styles.buttonText}>Animate Back</Text>
+        </TouchableOpacity> */}
+        <TouchableOpacity style={styles.button} onPress={this._MoveRight}>
+          <Text style={styles.buttonText}>Move in X Axis Right</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    // alignItems: "center",
     justifyContent: "center",
   },
-  box: {
+  animationView: {
     height: 140,
     width: 140,
     backgroundColor: "red",
   },
-  titleText: {
-    fontSize: 14,
-    lineHeight: 24,
+  buttonText: {
+    fontSize: 18,
+
     fontWeight: "bold",
+    color: "white",
+    padding: 12,
+  },
+  button: {
+    backgroundColor: "steelblue",
+    height: 45,
+    marginTop: 20,
+    alignSelf: "center",
   },
 });
-
-export default App;
