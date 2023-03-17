0097f1b0faae563664bfd6e10f6546af57bc5f10
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-17 10:13:12 +0530

    interpolate-practice reanimated

--- practice_animations/practice/App.js
+++ practice_animations/practice/App.js
@@ -1,36 +1,89 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Switch, Dimensions } from "react-native";
 
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
   withSpring,
+  useDerivedValue,
   withRepeat,
+  interpolateColor,
   useAnimatedGestureHandler,
+  interpolate,
 } from "react-native-reanimated";
 
-function App() {
-  const Move = useSharedValue(0);
+const Colors = {
+  dark: {
+    background: "#1E1E1E",
+    circle: "#252525",
+    text: "#F8F8F8",
+  },
+  light: {
+    background: "#F8F8F8",
+    circle: "#FFF",
+    text: "#1E1E1E",
+  },
+};
 
-  const animationStyles = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateX: Move.value }],
-    };
+const SWITCH_TRACK_COLOR = {
+  true: "rgba(256,0,256,0.2)",
+  false: "rgba(0,0,0,0.1)",
+};
+
+export default function App() {
+  const [theme, setTheme] = useState("light");
+
+  // const progress = useSharedValue(0);
+
+  const progress = useDerivedValue(() => {
+    //Basically useDerived value return a shared value, but enable us to deal with some computation
+    return theme === "dark" ? withTiming(1) : withTiming(0); // inorder to make it smooth we are using withTiming
+  }, [theme]); // since our progress value is depends on theme, put theme in dependencies.
+
+  const rStyle = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.background, Colors.dark.background]
+    );
+    return { backgroundColor };
   });
+
+  const rCircleStyle = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.circle, Colors.dark.circle]
+    );
+    return { backgroundColor };
+  });
+
+  const rTextStyle = useAnimatedStyle(() => {
+    const color = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.text, Colors.dark.text]
+    );
+    return { color };
+  });
+
   return (
-    <View style={styles.container}>
-      <Animated.View style={[styles.box, animationStyles]} />
-      <Button
-        title="Move"
-        onPress={() => {
-          Move.value = Math.random() * 255;
-        }}
-      />
-    </View>
+    <Animated.View style={[styles.container, rStyle]}>
+      <Animated.Text style={[styles.text, rTextStyle]}>THEME</Animated.Text>
+      <Animated.View style={[styles.circle, rCircleStyle]}>
+        <Switch
+          value={theme === "dark"}
+          onValueChange={(toggled) => setTheme(toggled ? "dark" : "light")}
+          trackColor={SWITCH_TRACK_COLOR}
+          thumbColor={"violet"}
+        />
+      </Animated.View>
+    </Animated.View>
   );
 }
 
+const SIZE = Dimensions.get("window").width * 0.7;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -38,11 +91,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  box: {
-    width: 100,
-    height: 100,
-    backgroundColor: "red",
+  circle: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: SIZE / 2,
+    shado: {
+      width: 0,
+      height: 20,
+    },
+    shadowRadius: 10,
+    shadowOpacity: 0.1,
+    elevation: 0.8,
+  },
+  text: {
+    fontSize: 70,
+    textTransform: "uppercase",
+    fontWeight: 700,
+    letterSpacing: 14,
+    marginBottom: 35,
   },
 });
-
-export default App;
