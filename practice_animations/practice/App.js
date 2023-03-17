0bfbe7b01af928be1cbc7812c4ed01b6d6416569
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-17 15:32:29 +0530

    tapGestureHandler

--- practice_animations/practice/App.js
+++ practice_animations/practice/App.js
@@ -1,90 +1,114 @@
+import React, { useRef, useCallback } from "react";
+
+import {
+  StyleSheet,
+  View,
+  Button,
+  Image,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
 import {
   GestureHandlerRootView,
-  PinchGestureHandler,
+  TapGestureHandler,
 } from "react-native-gesture-handler";
 import Animated, {
   useSharedValue,
-  withSpring,
-  withTiming,
   useAnimatedStyle,
-  useAnimatedGestureHandler,
+  withSpring,
+  withDelay,
 } from "react-native-reanimated";
 
-import { Image, StyleSheet, Dimensions } from "react-native";
-
-const { width, height } = Dimensions.get("window");
-const imageUri =
-  "https://images.unsplash.com/photo-1678933964986-22090877ccf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
-
-const AnimatedImage = Animated.createAnimatedComponent(Image); //creating image as animated to use inside pangesture handler
-
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 export default function App() {
-  const scale = useSharedValue(1);
-  const focalX = useSharedValue(0);
-  const focalY = useSharedValue(0);
+  const scale = useSharedValue(0);
+  const doubleTapRef = useRef();
+  const opacity = useSharedValue(1);
 
-  const Pinch = useAnimatedGestureHandler({
-    onActive: (event) => {
-      scale.value = event.scale;
-      focalX.value = event.focalX;
-      focalY.value = event.focalY;
-    },
-    onEnd: () => {
-      scale.value = withTiming(1);
-    },
-  });
+  const rStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: Math.max(scale.value, 0) }],
+  }));
 
-  const rStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: focalX.value },
-        { translateY: focalY.value },
-        { translateX: -width / 2 },
-        { translateY: -height / 2 },
-        { scale: scale.value },
+  const rTextStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+  }));
 
-        { translateX: -focalX.value },
-        { translateY: -focalY.value },
-        { translateX: width / 2 },
-        { translateY: height / 2 },
-      ],
-    };
-  });
+  const onDoubleTap = useCallback(() => {
+    scale.value = withSpring(1, undefined, (isFinished) => {
+      if (isFinished) {
+        scale.value = withDelay(500, withSpring(0));
+      }
+    });
+  }, []);
 
-  //focal point is actally on the center of the image ,and the point where we actually to zoom with pinch effect , we wnat
-  // that the image scaling happens in the focal point so we nee dto actually to use a trick , we want ,
-  //that the scale happened with the focal point but we know that the scale  will always happen at the center of the image
-  //so in order to pinch in the correct way, we need to center the image we need we need to fix the center of th image with focal point always and
-  // after we need to scale when everything is done we need to reset back our translate values
+  const onSingleTap = useCallback(() => {
+    opacity.value = withSpring(0, undefined, (isFinished) => {
+      if (isFinished) {
+        opacity.value = withDelay(500, withSpring(1));
+      }
+    });
+  }, []);
 
-  //to align focal point at the center of the image we need to translateX and translateY with the half of the image height and the image width.
-  const focalPointStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateX: focalX.value }, { translateY: focalY.value }],
-    };
-  });
   return (
-    //   //we are using gesture handler so we want to use Animated Image inside it. So above we want to create the image as amnimated
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <PinchGestureHandler onGestureEvent={Pinch}>
-        <Animated.View style={{ flex: 1 }}>
-          <AnimatedImage
-            style={[{ flex: 1 }, rStyle]}
-            source={{ uri: imageUri }}
-          />
-          <Animated.View style={[StyleSheet.focalPoint, focalPointStyle]} />
-        </Animated.View>
-      </PinchGestureHandler>
-    </GestureHandlerRootView>
+    <View style={styles.container}>
+      <GestureHandlerRootView>
+        <TapGestureHandler waitFor={doubleTapRef} onActivated={onSingleTap}>
+          <TapGestureHandler
+            maxDelayMs={250}
+            ref={doubleTapRef}
+            numberOfTaps={2}
+            onActivated={onDoubleTap}
+          >
+            <Animated.View>
+              <ImageBackground
+                source={{
+                  uri: "https://images.unsplash.com/photo-1600096194534-95cf5ece04cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80",
+                }}
+                style={styles.image}
+              >
+                <AnimatedImage
+                  source={{
+                    uri: "https://cdn-icons-png.flaticon.com/512/535/535183.png",
+                  }}
+                  style={[
+                    styles.image,
+                    {
+                      shadowOffset: { width: 0, height: 20 },
+                      shadowRadius: 35,
+                      shadowOpacity: 0.3,
+                    },
+                    rStyle,
+                  ]}
+                  resizeMode={"center"}
+                />
+              </ImageBackground>
+              <Animated.Text style={[styles.stars, rTextStyle]}>
+                ⭐⭐⭐⭐
+              </Animated.Text>
+            </Animated.View>
+          </TapGestureHandler>
+        </TapGestureHandler>
+      </GestureHandlerRootView>
+    </View>
   );
 }
 
+const { width: SIZE } = Dimensions.get("window");
+
 const styles = StyleSheet.create({
-  focalPoint: {
-    ...StyleSheet.absoluteFillObject,
-    width: 20,
-    height: 20,
-    backgroundColor: "blue",
-    borderRadius: 10,
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: SIZE,
+    height: SIZE,
+  },
+  stars: {
+    fontSize: 40,
+    textAlign: "center",
+    marginTop: 30,
   },
 });
