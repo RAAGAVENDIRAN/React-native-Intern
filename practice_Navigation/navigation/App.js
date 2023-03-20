78d5bfffbfa48b3048d3d29181cca03cb6a7f71b
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-20 11:57:17 +0530

    adding back button

--- practice_Navigation/navigation/App.js
+++ practice_Navigation/navigation/App.js
@@ -15,10 +15,12 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
