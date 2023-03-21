e75e74c02105ff8e6e8bf3a12ad18c7446af115d
Raagavendiran112 <78144615+Raagavendiran112@users.noreply.github.com>
2023-03-21 10:47:12 +0530

    Bottom Tab navigator

--- practice_Navigation/navigation/App.js
+++ practice_Navigation/navigation/App.js
@@ -3,45 +3,71 @@ import { View, Text, Button, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
+  return (
+    <>
+      <Tab.Navigator>
+        <Tab.Screen name="Feed" component={Feed} />
+        <Tab.Screen name="Messages" component={Messages} />
+      </Tab.Navigator>
+    </>
+  );
+}
+function LoginScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Text>Login Screen</Text>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate("settings")}
+      />
     </View>
   );
 }
 
-function LogoTitle() {
+function Feed({ navigation }) {
   return (
-    <Image
-      style={{ width: 50, height: 50 }}
-      source={{
-        uri: "https://reactnative.dev/img/tiny_logo.png",
-      }}
-    />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Login Screen</Text>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate("settings")}
+      />
+    </View>
+  );
+}
+
+function Messages({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Login Screen</Text>
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.navigate("login")}
+      />
+    </View>
+  );
+}
+
+function Settings() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Settings</Text>
+    </View>
   );
 }
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            headerTitle: (props) => <LogoTitle {...props} />,
-            headerRight: () => (
-              <Button
-                onPress={() => alert("This is a button!")}
-                title="Info"
-                color="#00cc00"
-              />
-            ),
-          }}
-        />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="login" component={LoginScreen} />
+        <Stack.Screen name="settings" component={Settings} />
       </Stack.Navigator>
     </NavigationContainer>
   );
